454cdf1fa417f0bef66df1284c662fa814170c37
Namit-Dhupar <namitdhupar2238@gmail.com>
2020-11-28 23:01:33 +0530

    additional func start

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,14 +3,14 @@ import { useSelector } from 'react-redux';
 import Showpdffull from './UI/Modal/ModalFull';
 import Showpdfphone from './UI/Modal/ModalMobile';
 import { makeStyles } from '@material-ui/core/styles';
-import {Tabs, 
+import {Tabs, TextField,
         Tab, 
         Box, 
         List, 
         ListItem, 
         Divider,
         Grid,
-        Hidden} from '@material-ui/core';
+        Hidden, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel} from '@material-ui/core';
 
 const TabPanel = (props) => {
     const { children, value, index, ...other } = props;
@@ -56,6 +56,9 @@ const TabPanel = (props) => {
 const ProductList = () => {
     const classes = useStyles();
     const [value, setValue] = useState(0);
+    const [radioValueHP, setradioValueHP] = useState('');
+    const [radioValueSize, setradioValueSize] = useState('');
+    const [radioValueMOC, setradioValueMOC] = useState('');
     const handleChange = (event, newValue) => {
         setValue(newValue);
       };
@@ -73,18 +76,80 @@ const tabPanel = productState.map((product,index) => {
           <List key={index}>
           <ListItem>
           <div key={el.id}>
+          <p><strong>{el.ItemName}</strong></p>
         <Grid container>
           <Grid item lg={3}>     
         <img src={el.ItemImageA} alt={index}/>
         <Hidden smDown>
             {(el.ItemImageB) ? <img src={el.ItemImageB} alt={index}/> : null}
         </Hidden>    
-            <p><strong>{el.ItemName}</strong></p>
          </Grid>         
          <Grid item lg={9}>
           <Hidden smDown>
           <Showpdffull title={el.ItemName} 
           pdf={el.ItemFile} isEnq={el.isEnquired} id={el.id} /> 
+          {el.isEnquired ?
+      <FormControl style={{marginTop: "10px"}} component="fieldset">
+      {el.ItemHPKW ? 
+      <div>
+      <FormLabel>Hp/Kw</FormLabel>
+      <RadioGroup row aria-label="position" value={radioValueHP} name="position"
+      onChange={e => setradioValueHP(e.target.value)} defaultValue="top"> 
+      {el.ItemHPKW.map((Hp,i)=>(
+      <FormControlLabel
+      key={i}
+      value={Hp}
+      control={<Radio color="primary" />}
+      label={Hp}
+      labelPlacement="start"
+    />      
+      ))} 
+      </RadioGroup>     
+      </div>
+      :
+      <div>
+      <FormLabel>Size(mm)</FormLabel>
+      <RadioGroup row aria-label="position" value={radioValueSize} name="position"
+      onChange={e => setradioValueSize(e.target.value)} defaultValue="top"> 
+      {el.ItemSize.map((Size,i)=>(
+      <FormControlLabel
+      key={i}
+      value={Size}
+      control={<Radio color="primary" />}
+      label={Size}
+      labelPlacement="start"
+    />      
+      ))} 
+      </RadioGroup>
+      </div>
+      }
+        <FormLabel>MOC</FormLabel>
+      <RadioGroup row aria-label="position2" name="position2" value={radioValueMOC}
+      onChange={e => e.target.value(setradioValueMOC)} defaultValue="top2">
+      {el.ItemMOC.map((MOC,i)=>(      
+        <FormControlLabel
+          key={i}
+          value={MOC}
+          control={<Radio color="secondary" />}
+          label={MOC}
+          labelPlacement="start"
+        />
+        ))} 
+        </RadioGroup>
+        <FormLabel>Other Details</FormLabel>
+        <TextField
+          required
+          fullWidth
+          value={el.ItemMessage}
+          name="Text"
+          multiline
+          rows={3}
+          variant="outlined"
+        />
+       </FormControl>
+       : null
+      }
+       
          <p>{el.ItemDescription}{' '}</p>   
          </Hidden>
          <Hidden mdUp>
